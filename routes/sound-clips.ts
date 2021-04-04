8e6d4e8fe56e78119861e81db8cfd79d9626bce6
angusho1 <angusho.77@gmail.com>
2021-04-03 22:50:09 -0700

    Fixed env issues and connected to mysql properly

--- routes/sound-clips.ts
+++ routes/sound-clips.ts
@@ -1,12 +1,25 @@
 import express from 'express';
 import { pool } from '../app';
+import SoundClip from '../models/soundClip';
+import Location from '../models/location';
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
     pool.query('SELECT * FROM SoundClips', (error, results, fields) => {
         if (error) throw error;
-        console.log(results);
-        console.log(fields);
+        const soundClips: SoundClip[] = results.map(res => {
+            const location: Location = { lat: res.latitude, lng: res.longitude, address: "" };
+            return {
+                title: res.title,
+                author: res.author,
+                description: res.description,
+                location,
+                date: res.date,
+                categories: [],
+                meta: null
+            }
+        });
+        console.log(soundClips);
     });
 
     res.send('Respond with a resource');
