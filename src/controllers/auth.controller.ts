cb7e30fd08abdc6f8b077f99eda53232c5a09bd4
angusho1 <angusho.77@gmail.com>
2021-04-26 22:30:48 -0700

    Added jwt token generation to signup endpoint

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,18 +1,31 @@
 import User from '../models/User';
 import UserService from '../services/user.service';
-
-async function login(req, res, next) {
-    res.send('Login');
-}
+import jwt from 'jsonwebtoken';
 
 async function signup(req, res, next) {
     const { email, password } = req.body;
     try {
-        const user: User = await UserService.createUser(email, password);
-        res.status(201).json({ user });
+        const userId: number = await UserService.createUser(email, password);
+        const token = createToken(userId);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: 60 * 1000 });
+        res.status(201).json({ userId });
     } catch (e) {
         next(e);
     }
+}
+
+async function login(req, res, next) {
+    const { email, password } = req.body;
+    try {
+        await UserService.loginUser(email, password);
+        res.status(200).json({});
+    } catch (e) {
+        next(e);
+    }
+}
+
+function createToken(userId) {
+    return jwt.sign({ id: userId }, process.env.JWT_SECRET, { expiresIn: 60 });
 }
 
 export default { login, signup };
--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,27 +3,27 @@ import User from '../models/User';
 import HttpError from '../utils/HttpError.util';
 import { db } from './db.service';
 
-export async function createUser(email: string, password: string): Promise<User> {
+export async function createUser(email: string, password: string): Promise<number> {
     const salt = await bcrypt.genSalt();
     const hashedPassword = await bcrypt.hash(password, salt);
     const sql: string = `INSERT INTO Users (email, hashed_password, creation_date)
                         VALUES (?, ?, now())`;
     const params = [email, hashedPassword];
+    let userId: number;
 
     try {
-        const result = await db.insert(sql, params);
+        await db.insert(sql, params);
+        userId = (await db.query('SELECT LAST_INSERT_ID()'))[0]['LAST_INSERT_ID()'];
     } catch (e) {
         if (e.code === 'ER_DUP_ENTRY') throw new HttpError(409, e.sqlMessage, e);
     }
 
-    return {
-        username: '',
-        email, 
-        creationDate: null, 
-        permission: '',
-        submissions: []
-    };
+    return userId;
+}
+
+export async function loginUser(email: string, password: string) {
+    return;
 }
 
 
-export default { createUser };
+export default { createUser, loginUser };
