2bbf9a6a159609ae5f91dcaefc33ccd9360b489b
angusho1 <angusho.77@gmail.com>
2021-05-10 22:41:16 -0700

    Fixed testing configuration and got test running

--- test/sound-clip.spec.ts
+++ test/sound-clip.spec.ts
@@ -1,22 +1,26 @@
 process.env.NODE_ENV = 'test';
+require('dotenv').config()
 
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../app';
 
-const should = chai.should();
-
+const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('Sound clips', () => {
 
     describe('/GET sound-clips', () => {
-        it('Should get all sound clips', () => {
+        it('Should get all sound clips', (done) => {
             chai.request(server)
                 .get('/sound-clips')
                 .end((err, res) => {
-                   console.log(res);
+                   expect(err).to.be.null;
+                   expect(res).to.have.status(200);
+                   done();
                 });
         });
     });
 });
+
+export {};
