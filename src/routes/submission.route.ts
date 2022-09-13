673eb1bef686b011da170054ffe33b3c7a460912
angusho1 <57027339+angusho1@users.noreply.github.com>
2022-09-12 22:58:27 -0700

    Add max file size to file upload

--- src/routes/submission.route.ts
+++ src/routes/submission.route.ts
@@ -1,15 +1,19 @@
 import express from 'express';
 import submissionController from '../controllers/submission.controller';
 import multer from 'multer';
-import CustomStorageEngine from '../utils/StorageEngine.util';
+import CustomStorageEngine, { MAX_FILE_UPLOAD_SIZE } from '../utils/StorageEngine.util';
 
 const storage = new CustomStorageEngine();
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage, limits: { fileSize: MAX_FILE_UPLOAD_SIZE } });
+
+const uploadFiles = upload.fields([
+    { name: 'recording', maxCount: 1},
+]);
 
 const router = express.Router();
 
 router.get('/submissions', submissionController.getSubmissions);
-router.post('/submissions', upload.single('recording'), submissionController.postSubmissions);
+router.post('/submissions', uploadFiles, submissionController.postSubmissions);
 
 export default router;
--- src/utils/StorageEngine.util.ts
+++ src/utils/StorageEngine.util.ts
@@ -3,23 +3,31 @@ import { ParamsDictionary } from 'express-serve-static-core';
 import { StorageEngine } from 'multer';
 import { ParsedQs } from 'qs';
 import fs from 'fs';
+import HttpError from './HttpError.util';
+
+export const MAX_FILE_UPLOAD_SIZE = 5 * (10 ** 6);
 
 export default class CustomStorageEngine implements StorageEngine {
 
     _handleFile(req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, file: Express.Multer.File, cb: (error?: any, info?: Partial<Express.Multer.File>) => void): void {
-        const path = `src/public/clips/${file.originalname}`;
-        console.log(file);
-    
-        const outStream = fs.createWriteStream(path);
-    
-        file.stream.pipe(outStream);
-        outStream.on('error', cb);
-        outStream.on('finish', function () {
-          cb(null, {
-            path,
-            size: outStream.bytesWritten
-          });
+      if (parseInt(req.headers["content-length"]) > MAX_FILE_UPLOAD_SIZE) {
+        cb(new HttpError(400, 'File size is too large (max allowed size per file is 5MB).'));
+        return;
+      }
+
+      const path = `src/public/clips/${file.originalname}`;
+      console.log(file);
+  
+      const outStream = fs.createWriteStream(path);
+  
+      file.stream.pipe(outStream);
+      outStream.on('error', cb);
+      outStream.on('finish', function () {
+        cb(null, {
+          path,
+          size: outStream.bytesWritten
         });
+      });
     }
     _removeFile(req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, file: Express.Multer.File, cb: (error: Error) => void): void {
         fs.unlink(file.path, cb);
