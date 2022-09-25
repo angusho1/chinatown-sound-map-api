29e4964dc4223229d541af1d2114967f8e664887
angusho1 <57027339+angusho1@users.noreply.github.com>
2022-09-24 21:35:06 -0700

    Allow file uploads to Azure Blob Storage

--- src/utils/StorageEngine.util.ts
+++ src/utils/StorageEngine.util.ts
@@ -3,29 +3,49 @@ import { ParamsDictionary } from 'express-serve-static-core';
 import { StorageEngine } from 'multer';
 import { ParsedQs } from 'qs';
 import fs from 'fs';
+import { BlobServiceClient, ContainerClient } from "@azure/storage-blob";
 
 export const MAX_FILE_UPLOAD_SIZE = 5 * (10 ** 6);
-const RECORDING_FOLDER = 'src/public/clips';
-const IMAGE_FOLDER = 'src/public/image-uploads';
+
+const blobServiceClient = BlobServiceClient.fromConnectionString(process.env.AZURE_STORAGE_CONNECTION_STRING);
+
+const RECORDINGS_CONTAINER = process.env.RECORDINGS_CONTAINER;
+const IMAGES_CONTAINER = process.env.IMAGES_CONTAINER;
+const recordingsContainerClient = blobServiceClient.getContainerClient(RECORDINGS_CONTAINER);
+const imagesContainerClient = blobServiceClient.getContainerClient(IMAGES_CONTAINER);
+
+const RECORDING_FIELDNAME = 'recording';
+const IMAGE_FIELDNAME = 'image';
 
 export default class CustomStorageEngine implements StorageEngine {
 
-    _handleFile(req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, file: Express.Multer.File, cb: (error?: any, info?: Partial<Express.Multer.File>) => void): void {
-      const path = `${file.fieldname === 'recording' ? RECORDING_FOLDER : IMAGE_FOLDER}/${file.originalname}`;
-      console.log(file);
-  
-      const outStream = fs.createWriteStream(path);
-  
-      file.stream.pipe(outStream);
-      outStream.on('error', cb);
-      outStream.on('finish', function () {
-        cb(null, {
-          path,
-          size: outStream.bytesWritten
-        });
+  _handleFile(req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, file: Express.Multer.File, cb: (error?: any, info?: Partial<Express.Multer.File>) => void): void {
+    console.log(file);
+
+    let containerClient: ContainerClient;
+    switch (file.fieldname) {
+      case RECORDING_FIELDNAME:
+        containerClient = recordingsContainerClient;
+        break;
+      case IMAGE_FIELDNAME:
+        containerClient = imagesContainerClient;
+      default:
+        break;
+    }
+
+    const blockBlobClient = containerClient.getBlockBlobClient(file.originalname);
+    blockBlobClient.uploadStream(file.stream)
+      .then(uploadResult => {
+        console.log('uploadResult', uploadResult);
+        cb(null);
+      })
+      .catch(e => {
+        console.log(`Error uploading file: ${file.originalname}`, e);
+        cb(e);
       });
-    }
-    _removeFile(req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, file: Express.Multer.File, cb: (error: Error) => void): void {
-        fs.unlink(file.path, cb);
-    }
+  }
+
+  _removeFile(req: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, file: Express.Multer.File, cb: (error: Error) => void): void {
+      fs.unlink(file.path, cb);
+  }
 }
