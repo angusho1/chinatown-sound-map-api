1174fdce25b84c11859542d5f4ca8e9d43ecba4e
angusho1 <57027339+angusho1@users.noreply.github.com>
2023-02-14 22:37:23 -0800

    Remove uses of render

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -5,10 +5,10 @@ import { Request, Response, NextFunction } from 'express';
 const router = express.Router();
 
 router.get('/', function(req: Request, res: Response, next: NextFunction) {
-  res.render('index', { title: 'Express' });
+  res.json({ message: 'Welcome' });
 });
 
-router.get('/login', (req: Request, res: Response) => res.render('login.html'));
-router.get('/auth-test', requireAuth, (req: Request, res: Response) => res.render('auth-test.html'));
+// router.get('/login', (req: Request, res: Response) => res.render('login.html'));
+// router.get('/auth-test', requireAuth, (req: Request, res: Response) => res.render('auth-test.html'));
 
 export default router;
